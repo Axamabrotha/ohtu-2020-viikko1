134baf4598d4da12da17f02aaf69a9a0f429d746
Axamabrotha <aku.hirvonen@gmail.com>
2020-02-29 13:47:57 +0200

    korjataan main checkstylen mukaisesti

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,27 +5,5 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("jees");  
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    System.out.println("jees"); 
-    
-    
     }
 }
