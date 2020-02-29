19b47855c208fb56b168d12ab0ce0ba8901aae53
Axamabrotha <aku.hirvonen@gmail.com>
2020-02-29 13:18:54 +0200

    checkstyle-hommat

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,5 +5,27 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+    System.out.println("jees");  
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    System.out.println("jees"); 
+    
+    
     }
 }
